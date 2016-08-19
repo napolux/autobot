d56fc89cd62fcfcb4e256358721f83a4a54d4d8e
Francesco Napoletano <napolux@gmail.com>
2016-08-19 17:39:25 +0200

    because I'm stupid

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/webhook', function (req, res) {
 function sendMessage(recipientId, message) {
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        qs: {access_token: process.env.MESSENGER_PAGE_ACCESS_TOKEN},
         method: 'POST',
         json: {
             recipient: {id: recipientId},
