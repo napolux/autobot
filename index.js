fe3b421cafc6035ff3997d1fd70ada4771e2007f
Francesco Napoletano <napolux@gmail.com>
2016-08-19 18:43:35 +0200

    Fixing bug in sender id

--- index.js
+++ index.js
@@ -25,22 +25,14 @@ app.get('/webhook', function (req, res) {
 
 // handler receiving messages
 app.post('/webhook', function (req, res) {
-
-  var data = JSONbig.parse(req.body);
-  messaging_events = data.entry[0].messaging;
-
-  for (i = 0; i < messaging_events.length; i++) {
-
-    event = data.entry[0].messaging[i];
-    sender = event.sender.id.toString();
-
-    if (event.message && event.message.text) {
-        sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+    var events = req.body.entry[0].messaging;
+    for (i = 0; i < events.length; i++) {
+        var event = events[i];
+        if (event.message && event.message.text) {
+            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+        }
     }
-
-  }
-
-  res.sendStatus(200);
+    res.sendStatus(200);
 });
 
 // generic function for sending messages
