2b7932cdc2ca4628aa7c7b0f2187b3d3cfb1c4ef
Francesco Napoletano <napolux@gmail.com>
2016-08-20 12:32:34 +0200

    Remove typing methods

--- index.js
+++ index.js
@@ -397,39 +397,6 @@ function sendReadReceipt(recipientId) {
     callSendAPI(messageData);
 }
 
-/*
- * Turn typing indicator on
- *
- */
-function sendTypingOn(recipientId) {
-    console.log("Turning typing indicator on");
-
-    var messageData = {
-        recipient: {
-            id: recipientId
-        },
-        sender_action: "typing_on"
-    };
-
-    callSendAPI(messageData);
-}
-
-/*
- * Turn typing indicator off
- *
- */
-function sendTypingOff(recipientId) {
-    console.log("Turning typing indicator off");
-
-    var messageData = {
-        recipient: {
-            id: recipientId
-        },
-        sender_action: "typing_off"
-    };
-
-    callSendAPI(messageData);
-}
 
 /*
  * Call the Send API. The message data goes in the body. If successful, we'll 
